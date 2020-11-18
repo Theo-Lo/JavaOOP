9ed12730610846053e63bb6e0556cbd8c41062cc
Theo-Lo <73676188+Theo-Lo@users.noreply.github.com>
2020-11-18 18:22:15 +0800

    Add files via upload

---
+++ JavaOOP/src/main.java
@@ -0,0 +1,8 @@
+import model.Student;
+
+public class main {
+    public static void main(String argc[]){
+        Student theo = new Student("Theo",22,"male", "458136");
+        System.out.println(theo);
+    }
+}
